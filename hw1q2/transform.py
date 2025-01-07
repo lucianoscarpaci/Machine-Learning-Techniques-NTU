5bf2a1dc78071c7d79950f0b89b3c4a54ba6d00d
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2025-01-07 14:26:02 -0500

    Add transformation points with their y labels

--- hw1q2/transform.py
+++ hw1q2/transform.py
@@ -4,6 +4,8 @@
 # Code the transformed points and their labels
 # original x = (x1, x2) y          transformed z = (phi1(x), phi2(x))
 # (1, 0)                -1              (1, -2)
+# Labels are supposedly -1, -1, -1, +1, +1, +1, +1
+# solve to verify the labels
 def phi1(x):
 	x1, x2 = x
 	return x2**2 - 2*x1 + 3
@@ -12,16 +14,18 @@ def phi2(x):
 	x1, x2 = x
 	return x1**2 - 2 * x2 - 3
 
+# data points and their labels y
 data_points = [
-    (1, 0),
-    (0, 1),
-    (0, -1),
-    (-1, 0),
-    (0, 2),
-    (0, -2),
-    (-2, 0)
+    ((1, 0), -1),
+    ((0, 1), -1),
+    ((0, -1), -1),
+    ((-1, 0), 1),
+    ((0, 2), 1),
+    ((0, -2), 1),
+    ((-2, 0), 1)
 ]
 
-transformed_points = [[phi1(x), phi2(x)] for x in data_points]
-for i, (original, transformed) in enumerate(zip(data_points, transformed_points)):
-    print(f"x_{i}: {original}, phi1 = {transformed[0]}, phi2 = {transformed[1]}")
+transformed_points_labels = [[phi1(x), phi2(x), y] for (x, y) in data_points]
+# Print the transformed points with their labels
+for i, (original, transformed) in enumerate(zip(data_points, transformed_points_labels)):
+    print(f"x_{i}: {original}, phi1 = {transformed[0]}, phi2 = {transformed[1]}, label = {transformed[2]}")
